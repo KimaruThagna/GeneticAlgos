5bf5e65c81fdcbab23751e02fa8ab0b1bb91fe98
Kimaru thagana <macbuntu@localhost.localdomain>
2019-08-06 14:45:00 +0300

    define the n-queen problem

--- n-queens.py
+++ n-queens.py
@@ -1,2 +1,9 @@
-import mlrose
+import mlrose # a library of common optimization problems such as n-queens ans TSP
 import numpy as np
+
+'''
+THE 8-QUEENS PROBLEM
+Having an 8x8 chess board and 8 queen pieces, what is the optimal way to place each piece such
+that no one piece can attack the other?
+Pre-requisite- Knowledge of chess that the queen piece can attack in any direction
+'''
